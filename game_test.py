9961e7243f2967d99a734aaa8111e59a267a4dbd
Ethan Mullins <54146188+EDMullins@users.noreply.github.com>
2024-03-08 10:56:16 -0600

    Update game_test.py
    Added a card total system

--- game_test.py
+++ game_test.py
@@ -41,6 +41,9 @@ cards = {1: '1', 2: '2', 3: '3',
 		13: 'King', 14: 'Ace'}
 card_key = random.randint(1, 14)
 card_queue = []
+card_total = 0
+card_total_text = smallfont.render('', True, white)
+
 running = True
 
 while running:
@@ -53,9 +56,18 @@ while running:
 			if width-150 <= mouse[0] <= width-10 and height-50 <= mouse[1] <= height-10:
 				stand_press = True
 			if width-300 <= mouse[0] <= width-160 and height-50 <= mouse[1] <= height-10:
+				#finds the value of the card and stores it in card_total
+				if card_key < 11:
+					card_total += card_key
+				elif card_key == 11 or card_key == 12 or card_key == 13 or card_key == 14:
+					card_total += 10
+				card_total_text = smallfont.render(str(card_total), True, white)
+				#adds the card to card_queue
 				card_queue.append(smallfont.render(cards[card_key], True, white))
 				card_key = random.randint(1, 14)
-	# fills the screen with a color 
+	
+	
+	#Everything below controls the visual aspect of my game
 	screen.fill((40,160,40)) 
 	
 	# stores the (x,y) coordinates into 
@@ -73,12 +85,16 @@ while running:
 		pygame.draw.rect(screen, color_light,[width-300, height-50, 140, 40])
 	else:
 		pygame.draw.rect(screen, color_dark,[width-300, height-50, 140, 40])
+	
 	# superimposing the text onto our button 
 	screen.blit(hit_text, (width-245, height-45))
 	screen.blit(stand_text , (width-115, height-45))	
     
+	#Displays the last card added to card queue (will eventually need to loop through my card_queue when I start displaying multiple cards at a time)
 	if len(card_queue) != 0:
 		screen.blit(card_queue[-1], (width/2, height/2))
     
+	#Displays the total values of all cards obtained
+	screen.blit(card_total_text, (width-40, height/2))
 	# updates the frames of the game 
-	pygame.display.update() 
+	pygame.display.update()  
